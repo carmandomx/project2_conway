b4ca04b12f98d079289a1ae8bcc258777de3be24
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-10-03 17:38:10 -0500

    asdf

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //si ij, si i-1j  
+let countgen = 0;
+
+function checkNeighbors() {}
+
+function alive() {}
+
+function dead() {}
+
+function clearBoard() {}
+
+function randomizer() {}
+
+function generateGrid() {}
+
+function pause() {
+    onclick
+}
+
+function play() {
+    onclick
+}
