9b3cb8c6d19634a20df8485ccf7ae74d2ee94b4f
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-10-04 17:04:59 -0500

    fix3

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-
+//We relate the container 
 const container = document.querySelector("#container");
 
+//Function that creates the grid
 function makeRows(rows, cols) {
+  //Connect the rows and cols variables with the CSS variables
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
+    //Every div is a cell
     let cell = document.createElement("div");
+    //Every cell is a grid-item
     container.appendChild(cell).className = "grid-item";
   };
 };
