1d8fff6e0fc54e99391c8e550e2cc5ed2c58ceea
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-10-04 14:32:34 -0500

    fix1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const container = document.getElementById("container");
+const container = document.querySelector("#container");
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
