04e2eb40709ac7cb97c49255c487097950e4bae2
RodrigoGarcia-Ksquare <al1609049@upy.edu.mx>
2022-10-05 09:41:37 -0500

    slider logic implemented, touch grid to mark

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 
-/* ////////////////////////TABLE GENERATION//////////////////*/
-/* Table size to be generated, ideally to be set by the user
-Since it is impossible to generate a n x m table by hand,
-it is necessary to make an itteration through JS */
-let row = 50;
-let column = 50;
+
+let row = 30;
+let column = 30;
 let sizePx = 10;
 let map = [];
 let generation = 0;
+
+let slider = document.querySelector('#sliderColumns')
+let gridSize = document.querySelector('.columns')
+
+gridSize.innerHTML = slider.value;
 
 document.addEventListener("keydown",(e) => {
     e.preventDefault()
     switch (e.keyCode) {
         case 39:
-            console.log("enter");
             updateStatus()
             break;
     
@@ -22,12 +23,29 @@ document.addEventListener("keydown",(e) => {
     }
 })
 
+slider.oninput = function() {
+    gridSize.innerHTML = this.value;
+    column = this.value
+    row = this.value
+    generateTable()
+}
+
+slider.addEventListener("input", function(){
+    var x = ((slider.value - slider.min) / (slider.max - slider.min) * 100 )
+    var color =`linear-gradient(90deg, rgb(117,252,117) ${x}%, rgb(214,214,214) ${x}%)`;
+    slider.style.background = color
+})
+
 generateTable()
 
+/* ////////////////////////TABLE GENERATION//////////////////*/
+/* Table size to be generated, ideally to be set by the user
+Since it is impossible to generate a n x m table by hand,
+it is necessary to make an itteration through JS */
+
 function generateTable(){
-    //An ID was given to this element since we don't want any other contect to modify the table size by "accident"
-    //Only by a grid modifier which the user can interact with
-    let html = "<table cellpadding=0 cellspacing=0 class='table_id'>"
+    //Grid can change its size by using the slider event.
+    let html = "<table cellpadding=0 cellspacing=0 class='table'>"
     for (let y = 0; y < row; y++) {
         html += "<tr>"
         for (let x = 0; x < column; x++) {
@@ -41,7 +59,7 @@ function generateTable(){
     html += "</table>"
     let container = document.querySelector('.tableContainer')
     container.innerHTML = html
-    let tablero = document.querySelector('.table_id')
+    let tablero = document.querySelector('.table')
     tablero.style.width = sizePx * column + "px"
     tablero.style.height = sizePx * row + "px"
 
@@ -97,12 +115,11 @@ function aliveCells(x,y){
 
 function updateStatus() {
     mapping()
+    console.log(map);
     for (let x = 0; x < column; x++) {
         for(let y = 0; y < column; y++){
             let alive = aliveCells(x,y)
             let cell = document.querySelector(`#cell-${x + "-" + y}`)
-            console.log(alive, cell)
-            // console.log(cell);
             if(map[x][y]){ //Its alive at this point
                 if(alive < 2 || alive > 3){ cell.style.background = "" }//Dies from overpopulation
             }
