cfe896df6128172c1c21d486fe7fba3e9c3234e4
“Dannaé <claudia.diaz@itksquare.edu.mx>
2022-10-09 21:25:38 -0500

    Revert "Merge pull request #14 from ClaudiaDiaz-Ksquare/hotFix_css"
    This reverts commit a11a0ad3919f820c21c216dd00d328f850868e83, reversing
    changes made to d7f7f07c19d85ef0e87022d0989ddadeafdebacb.

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ play.addEventListener("click", function () {
   // Show stop button and hide play button
   play.classList.add("hideIt");
   stop.classList.remove("hideIt");
-  //new image for "start" action.
+
   document.querySelector(".ico-gen").setAttribute("src", "./image/duck.png");
 });
 
