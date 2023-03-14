9ce6a36f904e4fee8be04234ce51385ced991d61
Alan Villanueva <alan.villanueva@itksquare.edu.mx>
2023-03-14 11:37:24 -0600

    Pause button added. Start button added. Disabled interacting in-game.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+//DOM elements
+const startButton=document.querySelector('.start');
+const pauseButton=document.querySelector('.pause');
+
+
 // Get canvas element and its context
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 // Number of rows and columns, and size of each cell
+let pause=false;
+let gameStarted=false
 const rows = 40;
 const columns = 40;
 const cellSize = canvas.width / rows;
@@ -36,8 +43,42 @@ const drawGrid = () => {
   }
 };
 
+function updateGrid() {
+  let newGrid = new Array(columns);
+  for (let i = 0; i < columns; i++) {
+    newGrid[i] = new Array(rows);
+  }
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      let neighbors = countNeighbors(i, j);
+      if (grid[i][j] == 0 && neighbors == 3) {
+        newGrid[i][j] = 1;
+      } else if (grid[i][j] == 1 && (neighbors < 2 || neighbors > 3)) {
+        newGrid[i][j] = 0;
+      } else {
+        newGrid[i][j] = grid[i][j];
+      }
+    }
+  }
+  grid = newGrid;
+}
+
+function countNeighbors(x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + columns) % columns;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
+}
+
 // Add click event listener to allow the user to toggle the their state and set the inial configuration
 canvas.addEventListener("click", (event) => {
+  if (!gameStarted){
   // Get clicked cell location
   const rect = canvas.getBoundingClientRect();
   const x = Math.floor((event.clientX - rect.left) / cellSize);
@@ -47,7 +88,16 @@ canvas.addEventListener("click", (event) => {
   grid[y][x] = grid[y][x] ? 0 : 1;
   // Redraw the cells
   drawGrid();
-});
+}});
 
 initGrid();
-drawGrid();
+
+pauseButton.addEventListener('click', ()=>{pause=!pause});
+startButton.addEventListener('click', ()=>{
+  gameStarted=true
+  setInterval(() => {
+      if(!pause){
+    updateGrid();
+    drawGrid();
+    }
+  }, 100);});
