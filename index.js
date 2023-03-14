c3dfa7b459c5551f7a5d75b6348d054018fd8d0b
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-03-14 14:54:10 -0400

    Clear board button added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //DOM elements
-const startButton=document.querySelector('.start');
-const pauseButton=document.querySelector('.pause');
-
+const startButton = document.querySelector(".start");
+const pauseButton = document.querySelector(".pause");
+const clearButton = document.querySelector(".clearBoard");
 
 // Get canvas element and its context
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 // Number of rows and columns, and size of each cell
-let pause=false;
-let gameStarted=false
+let pause = false;
+let gameStarted = false;
 const rows = 40;
 const columns = 40;
 const cellSize = canvas.width / rows;
@@ -43,7 +43,7 @@ const drawGrid = () => {
   }
 };
 
-function updateGrid() {
+const updateGrid = () => {
   let newGrid = new Array(columns);
   for (let i = 0; i < columns; i++) {
     newGrid[i] = new Array(rows);
@@ -61,9 +61,9 @@ function updateGrid() {
     }
   }
   grid = newGrid;
-}
+};
 
-function countNeighbors(x, y) {
+const countNeighbors = (x, y) => {
   let sum = 0;
   for (let i = -1; i < 2; i++) {
     for (let j = -1; j < 2; j++) {
@@ -74,31 +74,40 @@ function countNeighbors(x, y) {
   }
   sum -= grid[x][y];
   return sum;
-}
+};
 
 // Add click event listener to allow the user to toggle the their state and set the inial configuration
 canvas.addEventListener("click", (event) => {
-  if (!gameStarted){
-  // Get clicked cell location
-  const rect = canvas.getBoundingClientRect();
-  const x = Math.floor((event.clientX - rect.left) / cellSize);
-  const y = Math.floor((event.clientY - rect.top) / cellSize);
+  if (!gameStarted) {
+    // Get clicked cell location
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.floor((event.clientX - rect.left) / cellSize);
+    const y = Math.floor((event.clientY - rect.top) / cellSize);
 
-  // Toggle the cell's state
-  grid[y][x] = grid[y][x] ? 0 : 1;
-  // Redraw the cells
-  drawGrid();
-}});
+    // Toggle the cell's state
+    grid[y][x] = grid[y][x] ? 0 : 1;
+    // Redraw the cells
+    drawGrid();
+  }
+});
 
 initGrid();
 drawGrid();
 
-pauseButton.addEventListener('click', ()=>{pause=!pause});
-startButton.addEventListener('click', ()=>{
-  gameStarted=true
-  setInterval(() => {
-      if(!pause){
-    updateGrid();
-    drawGrid();
-    }
-  }, 100);});
+pauseButton.addEventListener("click", () => {
+  pause = !pause;
+});
+startButton.addEventListener("click", () => {
+  gameStarted = true;
+  if (!pause) {
+    setInterval(() => {
+      updateGrid();
+      drawGrid();
+    }, 100);
+  }
+});
+
+// Clear the board
+clearButton.addEventListener("click", () => {
+  initGrid();
+});
